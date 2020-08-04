0fcf9ae65ab8a73072894860244bebeddd25ba46
heyjohnny <rkd2016@gmail.com>
2020-08-04 20:20:21 +0900

    addcustomer

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +7,43 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
-app.get('/api/customers',(req,res)=>{
-    console.log("check111");
-    res.send([{
-        'id':1,
-        'image' : 'https://placeimg.com/64/64/1',
-        'name':'jame',
-        'birthday':'945685',
-        'gender':'male',
-        'job':'student'
-      },
-      {
-        'id':2,
-        'image' : 'https://placeimg.com/64/64/2',
-        'name':'olivia',
-        'birthday':'997124',
-        'gender':'female',
-        'job':'doctor'
-      },
-      {
-        'id':3,
-        'image' : 'https://placeimg.com/64/64/3',
-        'name':'Jamy',
-        'birthday':'000001',
-        'gender':'male',
-        'job':'student'
-      }
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
 
-    ]);
+const connection = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database
 });
+connection.connect();
+const multer = require('multer');
+const upload = multer({dest: './upload'}
+)
 
+app.get('/api/customers',(req,res)=>{
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows,fields) =>{
+            res.send(rows);
+        }
+    );
+});
+app.use('/image', express.static('./upload'));
+app.post('/api/customers', upload.single('image'), (req,res) =>{
+    let sql ='INSERT INTO CUSTOMER VALUES(null, ?,?,?,?,?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    console.log(image);
+    connection.query(sql,params,
+        (err,rows,fields)=>{
+            res.send(rows);
+        });
+});
 app.listen(port, () => console.log(`Listening on port ${port}`));
